daf588ec400c85e1e09ba9d934c696396800d75d
nushicoco <nushicarmon@gmail.com>
2017-06-04 10:52:48 +0300

    fix port for heroku

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
+const port = process.env.PORT || 3000
+
 MongoClient.connect('link-to-mongodb', (err, database) => {
     // ... start the server
 })
 
-app.listen(3000, function (){
+app.listen(port, function (){
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     })
