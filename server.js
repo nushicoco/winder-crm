fe82bac9e24044340a05f38cab6bc3583816a8fc
nushicoco <nushicarmon@gmail.com>
2017-06-04 11:43:26 +0300

    postgress try

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ const port = process.env.PORT || 3000
 // })
 
 app.listen(port, function (){
+
+    var pg = require('pg');
+
+    app.get('/db', function (request, response) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query('SELECT * FROM test_table', function(err, result) {
+                done();
+                if (err)
+                { console.error(err); response.send("Error " + err); }
+                else
+                { response.render('pages/db', {results: result.rows} ); }
+            });
+        });
+    });
+
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     })
