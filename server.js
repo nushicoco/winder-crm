80d415ed815865aee25df5119bb8fc3feeb3529c
nushicoco <nushicarmon@gmail.com>
2017-06-04 12:33:08 +0300

    connect to db before listening to port trying to use eslint (we can remove it)

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
+/* eslint-disable no-console */
 console.log('Starting Server......');
 
 const express = require('express');
 const app = express();
-// const MongoClient = require('mongodb').MongoClient
+const pg = require('pg');
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err)
+    { console.error(err); }
+    else
+    {
+        app.listen(port, function (){
 
-// MongoClient.connect('link-to-mongodb', (err, database) => {
-    // ... start the server
-// })
+            app.get('/', (req, res) => {
+                res.sendFile(__dirname + '/index.html');
+            });
 
-app.listen(port, function (){
-
-    var pg = require('pg');
-
-    app.get('/db', function (request, response) {
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('SELECT * FROM test_table', function(err, result) {
-                done();
-                if (err)
-                { console.error(err); response.send("Error " + err); }
-                else
-                { response.render('pages/db', {results: result.rows} ); }
+            app.post('/quotes', (req, res) => {
+                console.log('Hellooooooooooooooooo!');
             });
         });
-    });
 
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    })
+    }
 
-    app.post('/quotes', (req, res) => {
-        console.log('Hellooooooooooooooooo!');
-    })
+    // client.query('SELECT * FROM test_table', function(err, result) {
+    //     done();
+    //
+    // });
+
 });
