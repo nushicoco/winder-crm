b38c4e8bd1446c8517555979edcc9412fbf30320
nushicoco <nushicarmon@gmail.com>
2017-06-04 10:55:14 +0300

    remove mongo for upload

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ console.log('Starting Server......');
 
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 
 const port = process.env.PORT || 3000
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
+// MongoClient.connect('link-to-mongodb', (err, database) => {
     // ... start the server
-})
+// })
 
 app.listen(port, function (){
     app.get('/', (req, res) => {
