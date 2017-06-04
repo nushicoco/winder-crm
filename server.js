5c57ada70ace9e219644d7043460a9bf9220f268
nushicoco <nushicarmon@gmail.com>
2017-06-04 10:47:32 +0300

    check

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ console.log('Starting Server......');
 
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+    // ... start the server
+})
 
 app.listen(3000, function (){
-    console.log("listening to 3000");
-})
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
+
+    app.post('/quotes', (req, res) => {
+        console.log('Hellooooooooooooooooo!');
+    })
+});
