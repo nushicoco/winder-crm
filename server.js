29ceddc5fd1b7d323cf940e7df9c69b5f6d5e7e7
Dan Carmon <dan@carmon.org.il>
2017-06-08 06:21:40 +0700

    Introduced Sequelize

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ console.log('Starting Server......');
 
 const express = require('express');
 const app = express();
-var sqlite3 = require('sqlite3').verbose();
 const port = process.env.PORT || 3001;
 
 console.log("port ="  +port);
@@ -13,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-var db = new sqlite3.Database(process.env.DATABASE_URL || './db/winder.db');
+const { User, FrequentProblem  } = require('./models')
 
 // db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
 //     console.log(row.id + ": " + row.info);
@@ -27,26 +26,15 @@ var db = new sqlite3.Database(process.env.DATABASE_URL || './db/winder.db');
 
         app.get('/frequent_problem/:id', (req, res) => {
             var problemId = req.params.id;
-            db.all(`SELECT * FROM frequent_problems WHERE id = ${problemId}`, function(err, rows) {
-                console.log(err);
-                if (rows[0]) {
-                    res.send(JSON.stringify(rows[0]));
-                }else {
-                    res.sendStatus(400);
-                }
-            });
+            FrequentProblem.findById(problemId)
+                .then( (problem) => res.send(problem.toJSON()))
+                .catch( (error) => res.send(400))
         });
 
         app.get('/frequent_problems', (req, res) => {
-            console.log('frequent_problems');
-            db.all("SELECT * FROM frequent_problems", function(err, rows) {
-                // console.log(err);
-                res.send(JSON.stringify(rows));
-            });
+            FrequentProblem.findAll()
+                .then( all => res.send(all.map( item => item.toJSON())))
         });
     });
-
-
-    // db.close();
 
     // done();
