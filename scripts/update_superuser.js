83d713f8f00cb7a29c8555a13c528279730f3715
nushicoco <nushicarmon@gmail.com>
2017-06-12 13:07:39 +0300

    lint-ing

--- scripts/update_superuser.js
+++ scripts/update_superuser.js
@@ -1,13 +1,13 @@
 /**
  * Created by einavcarmon on 11/06/2017.
  */
-const { User } = require('../models')
+const { User } = require("../models");
 
 // process.argv.forEach(function (val, index, array) {
 //     console.log(index + ': ' + val);
 // });
 
-userEmail = process.argv[2];
+let userEmail = process.argv[2];
 
 if (!userEmail){
     console.log("plz supply a user email");
