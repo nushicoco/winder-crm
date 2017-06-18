f2084c5bdaf38ddc59daef91dd3c517072865e1a
nushicoco <nushicarmon@gmail.com>
2017-06-18 07:42:27 +0300

    some more css stuff update package.json with testing dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
               <img src={ process.env.PUBLIC_URL + '/img/logo_alpha.png'} className="App-logo" alt="logo" />
           </div>
           { !this.state.user && (
-              <div className="container login-container"> {Strings.header.loginHeader}
+              <div className="login-container"> {Strings.header.loginHeader}
                   <a className="inline" href="#" onClick={() => this.setState({showLogin: true})} >
                       <Button bsStyle="primary"> { Strings.header.loginAction } </Button>
                   </a>
@@ -57,7 +57,7 @@ class App extends Component {
           )}
 
           { this.state.user && (
-              <div className="container login-container">
+              <div className="login-container">
                   <p className="inline logged-in-header">{Strings.header.welcome}, {this.state.user.firstName} !</p> <span>
                         <a className="inline logout-header" href="#" onClick={() => this.logout()}>{Strings.header.logout}</a></span>
               </div>
