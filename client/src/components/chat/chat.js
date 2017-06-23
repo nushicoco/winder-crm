d50add8037ef5be6cf619eb4e3ac2b0de7163783
nushicoco <nushicarmon@gmail.com>
2017-06-23 15:51:32 +0300

    moved modal inside chattab now we can close modal if the user has arrived.

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -7,12 +7,12 @@ import { createChat } from '../../api';
 import { Tabs, Tab } from 'react-bootstrap';
 import io from 'socket.io-client'
 
-import { ButtonToolbar } from 'react-bootstrap'
-
-import { BackToFrequentBtn, NewTicketButton } from '../common';
+import { Modal, Button } from 'react-bootstrap'
+import Strings from '../../strings'
+import { BackToFrequentBtn } from '../common';
 
 import ChatTab from "./chatTab";
-import ChatNicknameModal from './chatModal'
+// import ChatNicknameModal from './chatModal'
 import './chat.css'
 
 export default class chatTab extends Component {
@@ -24,6 +24,7 @@ export default class chatTab extends Component {
             chats : [],
             client: props.user ? props.user.firstName : '',
             activeChatIndex: 0,
+            showModal:true,
             user: props.user
         };
 
@@ -36,9 +37,14 @@ export default class chatTab extends Component {
         // b/c we read the user from the cookie it takes time for it to
         // be set
         this.setState({user:nextProps.user});
-        if (this.state.user){
-            this.setState({client: this.state.user.firstName});
+        if (this.state.user)
+        {
+            this.setState({client:this.state.user.firstName});
         }
+    }
+
+    closeModal() {
+        this.setState({ showModal: false });
     }
 
     createChat(){
@@ -66,23 +72,37 @@ export default class chatTab extends Component {
         this.setState({activeChatIndex : index });
     }
 
-    updateNick(client) {
-        this.setState({client})
-    }
-
     render() {
         return (
             <div className="all-over">
-                <ChatNicknameModal show={!this.state.user || !this.state.user.firstName }
-                                   updateNick = {this.updateNick.bind(this)}
-                                   onExited = {this.createChat.bind(this)}
-                                   user = {this.state.user}
-                />
+                <Modal show={!this.state.user && this.state.showModal} onExited = {() => this.createChat()}>
+                    <Modal.Header>
+                        <Modal.Title>{Strings.chat.nicknameHeader}</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <input className="nickname" onChange={(e) => this.setState({client:e.target.value})}/>
+                        <Button bsStyle="primary"
+                                onClick={ () => {
+                                    {/*this.createChat();*/}
+                                    this.closeModal();
+                                } } >{Strings.chat.startChat}
+                        </Button>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <BackToFrequentBtn/>
+                    </Modal.Footer>
+                </Modal>
+
+                {/*<h2>client: I'm {this.state.client}</h2>*/}
+                {/*{this.state.user && <h2>User: I'm {this.state.user.firstName}</h2>}*/}
+
                 { this.state.chats.length == 0 && this.state.user && this.state.user.isSuperuser &&
                     <p>No chats pending</p>
                 }
-                { this.state.chats.length == 1 && this.state.client &&
-                    <ChatTab chatId={this.state.chats[0].id} client={ this.state.client } socket={ this.socket }></ChatTab>
+                { this.state.chats.length == 1 &&
+                    <ChatTab chatId={this.state.chats[0].id} client={ this.state.client || this.state.user.firstName} socket={ this.socket }></ChatTab>
                 }
                 { this.state.chats.length > 1 &&
                 <div className="tabs">
@@ -91,8 +111,8 @@ export default class chatTab extends Component {
                         {this.state.chats.map((chat, index) => {
                             return (
                                 <Tab key={chat.id} eventKey={index} title={chat.client}>
-                                    <ChatTab key={`chat-tab-${chat.id}`} chatId={chat.id} client={ chat.client }
-                                             socket={ this.socket } isSuperuser={this.state.user && this.state.user.isSuperuser}></ChatTab>
+                                    <ChatTab key={`chat-tab-${chat.id}`} chatId={chat.id} client={ this.state.client || this.state.user.firstName}
+                                             socket={ this.socket } isSuperuser={this.state.user && this.state.user.isSuperuser}/>
                                 </Tab>
                             )
                         })}
