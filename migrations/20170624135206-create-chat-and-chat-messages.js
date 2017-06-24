b0e69c8611504914e5e43679b81821b102a0ac7f
nushicoco <nushicarmon@gmail.com>
2017-06-24 19:28:15 +0300

    added migration

---
+++ migrations/20170624135206-create-chat-and-chat-messages.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+      queryInterface.createTable("chats", {
+          id: {
+              allowNull: false,
+              autoIncrement: true,
+              primaryKey: true,
+              type: Sequelize.INTEGER
+          },
+          status: {
+              type: Sequelize.STRING,
+              validate: {
+                  isIn: [['active', 'closed']]
+              }
+          },
+
+          client: Sequelize.TEXT,
+          clientId: Sequelize.TEXT,
+
+          createdAt: {
+              allowNull: false,
+              type: Sequelize.DATE
+          },
+          updatedAt: {
+              allowNull: false,
+              type: Sequelize.DATE
+          }
+      });
+
+      queryInterface.sequelize.query([
+                  `CREATE TABLE chatMessages ( 
+                                      id            INTEGER PRIMARY KEY NOT NULL, 
+                                      client        TEXT, 
+                                      clientId      TEXT,
+                                      text          TEXT,
+                                      chat_id       INTEGER,
+                                      FOREIGN KEY (chat_id) REFERENCES chats(id)
+                                      );`,
+              ].join(''),
+              { raw: true });
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+      return queryInterface.dropTable('chats');
+      return queryInterface.dropTable('chatMessages');
+
+  }
+};
