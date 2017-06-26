6cfdaa628615ce76489db3b28618b5e06c377fa0
Dan Carmon <dan@carmon.org.il>
2017-06-26 17:51:24 +0700

    standard-izing

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -2,98 +2,96 @@
  * Created by einavcarmon on 19/06/2017.
  */
 
-import React, { Component } from 'react';
-import { createChat } from '../../api';
+import React, { Component } from 'react'
+import { createChat } from '../../api'
 
-import Strings from '../../strings'
 import LoadingBox from '../loadingBox'
-import { BackToFrequentBtn } from '../common';
+import { BackToFrequentBtn } from '../common'
 
-import ChatTab from "./chatTab";
+import ChatTab from './chatTab'
 import EnterNicknameWindow from './EnterNickname'
 import './chat.css'
 import createSocket from './createSocket'
 
 export default class chatTab extends Component {
+  constructor (props) {
+    super(props)
 
-    constructor (props) {
-        super(props);
+    this.state = {
+      isLoading: true,
+      chatId: null,
+      nickname: null
+    }
+    this.socket = createSocket()
+  }
 
-        this.state = {
-            isLoading: true,
-            chatId: null,
-            nickname: null
-        };
-        this.socket = createSocket()
+  getClientName () {
+    return this.props.user ? this.props.user.firstName : this.state.nickname
+  }
+
+  componentWillMount () {
+    // In case the component was initialized with a user:
+    this.props.user && this.createChat()
+  }
+
+  componentDidUpdate (prevProps) {
+    // In case the component received a user after initialization:
+    if (!prevProps.user && this.props.user) {
+      this.createChat()
+    }
+  }
+
+  createChat () {
+    createChat(this.getClientName())
+
+      .then((chat) => {
+        this.setState({
+          isLoading: false,
+          chatId: chat.id
+        })
+      })
+
+      .catch((err) => {
+        this.setState({
+          isLoading: 'error'
+        })
+        console.error(err)
+      })
+  }
+
+  handleNicknameSet (nickname) {
+    this.setState({nickname})
+    this.createChat()
+  }
+
+  renderChat () {
+    if (!this.state.chatId) {
+      return null
     }
 
-    getClientName(){
-        return this.props.user ? this.props.user.firstName : this.state.nickname
-    }
+    return (
+      <ChatTab
+        chatId={this.state.chatId}
+        clientName={this.getClientName()}
+        socket={this.socket} />
+    )
+  }
 
-    componentWillMount() {
-        // In case the component was initialized with a user:
-        this.props.user && this.createChat()
-    }
+  render () {
+    const hasNoUser = this.props.userQueryCompleted && !this.props.user
+    const hasNoNickname = !this.state.nickname
+    return (
+      <div className='all-over'>
+        <EnterNicknameWindow
+          onNicknameSet={this.handleNicknameSet.bind(this)}
+          show={hasNoUser && hasNoNickname} />
 
-    componentDidUpdate(prevProps) {
-        // In case the component received a user after initialization:
-        if (!prevProps.user && this.props.user) {
-            this.createChat()
-        }
-    }
+        <LoadingBox show={this.state.isLoading}>
+          { this.renderChat() }
+        </LoadingBox>
 
-    createChat = () => {
-        createChat(this.getClientName())
-
-            .then( (chat) =>  {
-                this.setState({
-                    isLoading: false,
-                    chatId: chat.id
-                })
-            })
-
-            .catch( (err) => {
-                this.setState({
-                    isLoading: 'error'
-                })
-                console.error(err)
-            })
-    }
-
-    handleNicknameSet = (nickname) => {
-        this.setState({nickname})
-        this.createChat()
-    }
-
-    renderChat(){
-        if (!this.state.chatId) {
-            return null
-        }
-
-        return (
-            <ChatTab
-                chatId={ this.state.chatId }
-                clientName={ this.getClientName()}
-                socket={ this.socket } />
-        )
-    }
-
-    render() {
-        const hasNoUser = this.props.userQueryCompleted && !this.props.user
-        const hasNoNickname = !this.state.nickname
-        return (
-            <div className="all-over">
-                <EnterNicknameWindow
-                    onNicknameSet={ this.handleNicknameSet }
-                    show={ hasNoUser && hasNoNickname }/>
-
-                <LoadingBox show={ this.state.isLoading }>
-                    { this.renderChat() }
-                </LoadingBox>
-
-                <BackToFrequentBtn/>
-            </div>
-        )
-    }
+        <BackToFrequentBtn />
+      </div>
+    )
+  }
 }
