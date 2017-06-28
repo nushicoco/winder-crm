5d832dc901c66680a85e62cacf12d823584eedc1
Dan Carmon <dan@carmon.org.il>
2017-06-28 11:33:47 +0700

    route-analytics: fixed duplicate recording

--- client/src/components/RouteAnalytics.js
+++ client/src/components/RouteAnalytics.js
@@ -2,10 +2,21 @@ import React from 'react'
 import { recordEvent } from '../analytics'
 
 export default class RouteAnalytics extends React.Component {
+  componentWillMount () {
+    if (this.props.userQueryCompleted) {
+      this.recordRouteEvent(this.props)
+    }
+  }
   componentWillReceiveProps (newProps) {
-    const user = newProps.user
+    if (newProps.location.pathname !== this.props.location.pathname ||
+        newProps.user !== this.props.user) {
+      this.recordRouteEvent(newProps)
+    }
+  }
+  recordRouteEvent (props) {
+    const user = props.user
     const isSuperuser = user && user.isSuperuser
-    const path = newProps.location.pathname
+    const path = props.location.pathname
     let event = isSuperuser
         ? `Route (admin) ${path}`
         : `Route ${path}`
