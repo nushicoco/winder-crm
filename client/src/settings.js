922833d7e582a53ea5b4fff73b12a80cfccb24cb
Dan Carmon <dan@carmon.org.il>
2017-07-07 18:46:02 +0700

    settings.json.example added + signup length validation from settings

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,7 +1,11 @@
 const customerSettings = require('./settings.json') // ./settings.json is customer-specific
-export default {
-  logo: customerSettings.logo || process.env.PUBLIC_URL + '/img/logo.png',
-  rooms: [].concat(customerSettings.rooms || []),
-  subjects: [].concat(customerSettings.subjects || []),
-  techPhone: customerSettings.techPhone || ''
+const DEFAULT_SETTINGS = {
+  minNameLength: 1,
+  minPasswordLength: 1,
+  logo: process.env.PUBLIC_URL + '/img/logo.png',
+  rooms: [],
+  subjects: customerSettings.subjects,
+  techPhone: customerSettings.techPhone
 }
+
+export default Object.assign(DEFAULT_SETTINGS, customerSettings)
